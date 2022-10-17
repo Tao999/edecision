ca6c751a81fcfad47c3516c3d757efd69e2d1f7d
Paul Maurette <paul.maurette@gmail.com>
2022-10-17 13:40:11 +0200

    start project

---
+++ src/test/java/com/example/edecision/EDecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edecision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDecisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
